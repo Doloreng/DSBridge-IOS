d069b0cb17cb8499b6e3f5eb379d40f4b1e20f00
kunpenghong <576012206@qq.com>
2025-09-04 11:21:41 +0800

    修复共享的逻辑

--- dsbridge/DSBridgeMethodProxy.m
+++ dsbridge/DSBridgeMethodProxy.m
@@ -15,104 +15,112 @@
 
 @implementation DSBridgeMethodProxy
 
+
 - (instancetype)init {
-    self = [super init];
-    if (self) {
-        _methodHandlers = [NSMutableDictionary dictionary];
-    }
-    return self;
+   self = [super init];
+   if (self) {
+       _methodHandlers = [NSMutableDictionary dictionary];
+   }
+   return self;
 }
 
 - (void)registerMethod:(NSString *)methodName withHandler:(id)handler {
-    if (methodName == nil || handler == nil) {
-        return;
-    }
-    
-    // Store the handler
-    [self.methodHandlers setObject:handler forKey:methodName];
-    
-    // Add the method to the class dynamically
-    class_addMethod([self class], 
-                   NSSelectorFromString(methodName), 
-                   (IMP)dynamicMethodIMP, 
-                   "v@:@");
+   if (methodName == nil || handler == nil) {
+       return;
+   }
+   
+   // Store the handler
+   [self.methodHandlers setObject:handler forKey:methodName];
+   
+   // 为方法添加两个参数的方法签名，参考LXSZJSBAccoutAPIs的实现
+   // 格式: methodName:params:responseCallback
+   NSString *fullMethodName = [NSString stringWithFormat:@"%@::", methodName];
+   SEL methodSel = NSSelectorFromString(fullMethodName);
+   
+   if (!class_getInstanceMethod([self class], methodSel)) {
+       class_addMethod([self class],
+                      methodSel,
+                      (IMP)dynamicMethodIMP,
+                      "v@:@@");
+   }
 }
 
 - (BOOL)hasMethod:(NSString *)methodName {
-    return [self.methodHandlers objectForKey:methodName] != nil;
+   return [self.methodHandlers objectForKey:methodName] != nil;
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+   NSString *methodName = NSStringFromSelector(aSelector);
+   
+   // 检查是否是 methodName:: 格式的方法
+   if ([methodName hasSuffix:@"::"]) {
+       NSString *baseMethodName = [methodName stringByReplacingOccurrencesOfString:@"::" withString:@""];
+       if ([self hasMethod:baseMethodName]) {
+           return YES;
+       }
+   }
+   
+   return [super respondsToSelector:aSelector];
 }
 
 - (NSArray<NSString *> *)registeredMethodNames {
-    return [self.methodHandlers allKeys];
+   return [self.methodHandlers allKeys];
 }
 
-// Dynamic method implementation that forwards calls to the stored handler
-static id dynamicMethodIMP(id self, SEL _cmd, ...) {
+// 动态方法实现，处理 methodName:params:responseCallback 格式的调用
+static void dynamicMethodIMP(id self, SEL _cmd, id params, id responseCallback) {
     DSBridgeMethodProxy *proxy = (DSBridgeMethodProxy *)self;
     NSString *methodName = NSStringFromSelector(_cmd);
+    // 移除末尾的 :: 得到基础方法名
+    methodName = [methodName stringByReplacingOccurrencesOfString:@"::" withString:@""];
+    
     id handler = [proxy.methodHandlers objectForKey:methodName];
     
-    if (handler) {
-        // Get the arguments
-        va_list args;
-        va_start(args, _cmd);
-        
-        // For simplicity, we'll handle common cases
-        // In a real implementation, you might want to parse the method signature
-        // and handle different argument types properly
-        
-        // Try to call the handler as a block
-        if ([handler isKindOfClass:NSClassFromString(@"NSBlock")]) {
-            // This is a simplified implementation
-            // In practice, you'd need to handle different block signatures
-            return handler;
-        }
-        
-        va_end(args);
+    if (handler && [handler isKindOfClass:NSClassFromString(@"NSBlock")]) {
+        // 调用block，传递参数和回调
+        void (^block)(id, id) = (void (^)(id, id))handler;
+        block(params, responseCallback);
     }
-    
-    return nil;
-}
-
-// Override forwardInvocation to handle method calls
-- (void)forwardInvocation:(NSInvocation *)invocation {
-    NSString *methodName = NSStringFromSelector([invocation selector]);
-    id handler = [self.methodHandlers objectForKey:methodName];
-    
-    if (handler) {
-        // Handle the invocation based on the handler type
-        if ([handler isKindOfClass:NSClassFromString(@"NSBlock")]) {
-            // Handle block-based handlers
-            [self handleBlockInvocation:invocation withHandler:handler];
-        } else {
-            // Handle object-based handlers
-            [invocation invokeWithTarget:handler];
-        }
-    } else {
-        [super forwardInvocation:invocation];
-    }
-}
-
-- (void)handleBlockInvocation:(NSInvocation *)invocation withHandler:(id)handler {
-    // This is a simplified implementation
-    // In practice, you'd need to parse the block signature and handle arguments properly
-    // For now, we'll just log that the method was called
-    
-    NSString *methodName = NSStringFromSelector([invocation selector]);
-    NSLog(@"DSBridge: Method '%@' called with handler", methodName);
-    
-    // You could implement more sophisticated argument handling here
-    // based on the block signature and invocation arguments
 }
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
-    NSString *methodName = NSStringFromSelector(aSelector);
-    if ([self hasMethod:methodName]) {
-        // Return a generic method signature
-        // In practice, you might want to analyze the block signature
-        return [NSMethodSignature signatureWithObjCTypes:"@@:"];
-    }
-    return [super methodSignatureForSelector:aSelector];
+   NSString *methodName = NSStringFromSelector(aSelector);
+   
+   // 检查是否是 methodName:: 格式的方法
+   if ([methodName hasSuffix:@"::"]) {
+       NSString *baseMethodName = [methodName stringByReplacingOccurrencesOfString:@"::" withString:@""];
+       if ([self hasMethod:baseMethodName]) {
+           // 返回 v@:@@ 签名，对应 void methodName:(id)params :(id)responseCallback
+           return [NSMethodSignature signatureWithObjCTypes:"v@:@@"];
+       }
+   }
+   
+   return [super methodSignatureForSelector:aSelector];
 }
+
+- (void)forwardInvocation:(NSInvocation *)invocation {
+   NSString *methodName = NSStringFromSelector([invocation selector]);
+   
+   // 检查是否是 methodName:: 格式的方法
+   if ([methodName hasSuffix:@"::"]) {
+       NSString *baseMethodName = [methodName stringByReplacingOccurrencesOfString:@"::" withString:@""];
+       if ([self hasMethod:baseMethodName]) {
+           id params = nil;
+           id responseCallback = nil;
+           [invocation getArgument:&params atIndex:2];
+           [invocation getArgument:&responseCallback atIndex:3];
+           
+           id handler = [self.methodHandlers objectForKey:baseMethodName];
+           if ([handler isKindOfClass:NSClassFromString(@"NSBlock")]) {
+               void (^block)(id, id) = (void (^)(id, id))handler;
+               block(params, responseCallback);
+           }
+           return;
+       }
+   }
+   
+   [super forwardInvocation:invocation];
+}
+
 
 @end
